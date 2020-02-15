1120f0afa40e0ea7293fe48c9802b44ef8e5bd1c
Aditya Kannan <akannan2@andrew.cmu.edu>
2020-02-15 07:59:05 -0500

    data on all schools

--- depIndex.py
+++ depIndex.py
@@ -2,28 +2,51 @@ from azure.ai.textanalytics import single_analyze_sentiment, single_extract_key_
 from scipy.special import expit
 import pickle
 import pandas as pd
+#from multiprocessing.dummy import Pool
+from tqdm import tqdm
 
 key = "24f1f63604d14d16b34aa2849540e6b3"
 endpoint = "https://uplyft.cognitiveservices.azure.com/"
 
 # Text Sentiment Analysis for one confesstion
 def sentiment_analysis(document, endpoint=endpoint, key=key):
+    # print("started")
+    # print("sending rqs")
     response = single_analyze_sentiment(endpoint=endpoint, credential=key, input_text=document)
     pos, neg = response.document_scores.positive, response.document_scores.negative
-    return expit((neg - pos) * 4)
+    # print("yay")
+    return (neg - pos) * 4
+        # print("oops")
+        # return None
+
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
 
 # Providing a depression index over a list of confessions
 def depIndex(confList):
     total = 0
     cnt = 0
-    for confession in confList:
+    for confession in chunks(confList, 10):
         try: 
-            total += sentiment_analysis(confession)
+            total += sentiment_analysis('\n'.join(confession))
             cnt += 1
         except:
             continue
 
-    return total / cnt
+    try: 
+        return expit(total / cnt)
+    except:
+        return None
+
+    # results = []
+    # p = Pool(1)
+    # results = p.map(sentiment_analysis, confList)
+
+    # results = list(filter(lambda x: x is not None, results))
+
+    # return sum(results) / len(results)
+
 
 if __name__ == "__main__":
     # test on CMU confessions
@@ -40,7 +63,7 @@ if __name__ == "__main__":
     data = {"School": [], "Index": []}
     with open(fpath, "rb") as f:
         schoolDict = pickle.load(f)
-        for school, confessions in schoolDict.items():
+        for school, confessions in tqdm(schoolDict.items()):
             conf_processed = confessions[1:100]
             dep = depIndex(conf_processed)
             print(school + ": " + str(dep))
