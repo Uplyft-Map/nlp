6c3a42b8a528e25efc2028e5b6621ccf230a549d
Aditya Kannan <akannan2@andrew.cmu.edu>
2020-02-15 07:17:03 -0500

    latest test cases

--- depIndex.py
+++ depIndex.py
@@ -1,6 +1,7 @@
 from azure.ai.textanalytics import single_analyze_sentiment, single_extract_key_phrases
 from scipy.special import expit
 import pickle
+import pandas as pd
 
 key = "24f1f63604d14d16b34aa2849540e6b3"
 endpoint = "https://uplyft.cognitiveservices.azure.com/"
@@ -14,15 +15,38 @@ def sentiment_analysis(document, endpoint=endpoint, key=key):
 # Providing a depression index over a list of confessions
 def depIndex(confList):
     total = 0
+    cnt = 0
     for confession in confList:
-        total += sentiment_analysis(confession)
+        try: 
+            total += sentiment_analysis(confession)
+            cnt += 1
+        except:
+            continue
 
-    return total / len(confList)
+    return total / cnt
 
 if __name__ == "__main__":
     # test on CMU confessions
+    """
     fpath = "tests/cmu_confessions.pickle"
     with open(fpath, "rb") as f:
         confessions = pickle.load(f)
         confessions = confessions[1:]
         print(depIndex(confessions))
+    """
+
+    # test on all colleges' confessions
+    fpath = "../fb-scraper/school_confessions.pickle"
+    data = {"School": [], "Index": []}
+    with open(fpath, "rb") as f:
+        schoolDict = pickle.load(f)
+        for school, confessions in schoolDict.items():
+            conf_processed = confessions[1:100]
+            dep = depIndex(conf_processed)
+            print(school + ": " + str(dep))
+            data["School"].append(school)
+            data["Index"].append(dep)
+
+    df_schools = pd.DataFrame(data)
+    print(df_schools)
+    df_schools.to_csv("school_indices.csv")
